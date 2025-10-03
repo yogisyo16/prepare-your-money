76a6cc6f05d18b5c053df5ad3237787797720987
yogisyo16 <yogisyo16@gmail.com>
2025-10-03 16:24:40 +0700

    Update: Navbar

--- src/components/NavbarP/navbarDesktopPm.vue
+++ src/components/NavbarP/navbarDesktopPm.vue
@@ -40,7 +40,7 @@ defineProps({
                         class="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                         active-class="border-indigo-500 text-gray-900"
                     >
-                        Transactions Try
+                        Transactions
                     </RouterLink>
                     <RouterLink 
                         :to="reports"
