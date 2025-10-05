64af07c7374ab968eeb55d6f232f7dd96a5818df
yogisyo16 <yogisyo16@gmail.com>
2025-10-05 22:29:25 +0700

    Update: Dashboard - Now income and expenses have button to add details for now not going anywhere

--- src/components/Dashboard/DashboardMoney.vue
+++ src/components/Dashboard/DashboardMoney.vue
@@ -15,32 +15,46 @@
         cardMonth: { type: String, required: false },
         cardYear: { type: String, required: false },
         cardCVV: { type: String, required: false },
+
+        // Dashboard information
+        totalSpending: { type: Number, required: false },
+        totalIncome: { type: Number, required: false },
+        totalBalance: { type: Number, required: false },
+
+        // Button Interaction
+        buttonAddIncome: { type: String, required: false },
+        buttonAddDetailsSpending: { type: String, required: false },
     })
 </script>
 
 <template>
     <div class="flex flex-col items-center md:items-start gap-2 md:gap-10">
         <div class="flex flex-col md:flex-row gap-1 md:gap-4">
-            <div class="flex flex-col items-start">
+            <div class="flex flex-col items-start gap-4">
                 <h1 class="text-2xl font-bold">Dashboard</h1>
-                <h1 class="text-2xl font-bold">Card</h1>
-                <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/src/assets/char-face.jpg" alt="">
-                    <div class="flex flex-col justify-between p-4 leading-normal">
-                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ cardName }}</h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ cardNumber }}</p>
+                <div class="flex flex-row gap-2">
+                    <div class="flex flex-col items-start border-2 rounded-2xl p-4">
+                        <h1 class="text-2xl font-bold">Card</h1>
+                        <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow-sm md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+                            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/src/assets/char-face.jpg" alt="">
+                            <div class="flex flex-col justify-between p-4 leading-normal">
+                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ cardName }}</h5>
+                                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ cardNumber }}</p>
+                            </div>
+                        </a>
                     </div>
-                </a>
+                    <div class="flex flex-col items-start border-2 rounded-2xl p-4">
+                        <h1 class="text-2xl font-bold">Income</h1>
+                        <p>Rp. {{ totalIncome }}</p>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="{{ buttonAddIncome }}">Add</button>
+                    </div>
+                    <div class="flex flex-col items-start border-2 rounded-2xl p-4">
+                        <h1 class="text-2xl font-bold">Expenses</h1>
+                        <p>Rp. {{ totalSpending }}</p>
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="{{ buttonAddDetailsSpending }}">Add Details</button>
+                    </div>
+                </div>
             </div>
-            <div class="flex flex-row items-center">
-                <h1 class="text-2xl font-bold">Income</h1>
-            </div>
-            <div class="flex flex-row items-center">
-                <h1 class="text-2xl font-bold">Expenses</h1>
-            </div>
-        </div>
-        <div class="flex flex-row">
-            <h1 class="text-2xl font-bold">My Name</h1>
         </div>
     </div>
 </template>
